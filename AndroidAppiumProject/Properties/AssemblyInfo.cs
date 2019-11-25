6f24c91f2cf0c0b81d26f65e27ca57b23309dbbc
magdalenewa <57532484+magdalenewa@users.noreply.github.com>
2019-11-25 17:07:40 +0100

    Add project files.

---
+++ AndroidAppiumProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AndroidAppiumProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AndroidAppiumProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("945d8b04-c9fc-4605-9a7b-d623d589de79")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AndroidAppiumProject/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace AndroidAppiumProject
+{
+  
+
+   public class CSEConnect
+    {
+        private static AndroidDriver<AppiumWebElement> driver;
+        private static AppiumLocalService appiumLocalService;
+
+
+        public static void Main()
+        {
+           // appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+           // appiumLocalService.Start();
+            var appiumOptions = new AppiumOptions();
+
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "device");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "9.0");
+            appiumOptions.AddAdditionalCapability(AndroidMobileCapabilityType.AppPackage, "com.draeger.connect.cse");
+            appiumOptions.AddAdditionalCapability(AndroidMobileCapabilityType.AppActivity, "com.draeger.connect.cse.MainActivity");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.NoReset, true);
+            //new Uri("http://127.0.0.1:4723/wd/hub")
+            driver = new AndroidDriver<AppiumWebElement>(appiumLocalService, appiumOptions);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            AppiumWebElement approveBox = driver.FindElementByAndroidUIAutomator("new UiSelector().textContains(\"Approve\");");
+            approveBox.Click();
+
+            // driver.FindElementByAndroidUIAutomator("new UiSelector().textContains(\"Approve\");");
+            // wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("Jobs")));
+            // driver.FindElementByAndroidUIAutomator("new UiSelector().textContains(\"Approve\")");
+            //driver.CloseApp();
+
+
+        }
+
+
+        public void TestInitalize()
+        {
+            driver.LaunchApp();
+        }
+
+        public void TestCleanup()
+        {
+            driver.CloseApp();
+        }
+    }
+}
